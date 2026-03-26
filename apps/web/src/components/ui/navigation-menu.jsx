7daa970b76b790f5b8ce00d116523e74be4d1a5b
medamineaitaich <medamineaitaich@gmail.com>
2026-03-26 23:33:41 +0100

    chore(ui): update navigation-menu spacing

--- apps/web/src/components/ui/navigation-menu.jsx
+++ apps/web/src/components/ui/navigation-menu.jsx
@@ -84,6 +84,8 @@ const NavigationMenuIndicator = React.forwardRef(({ className, ...props }, ref) 
       className
     )}
     {...props}>
+
+
     <div
       className="relative top-[60%] h-2 w-2 rotate-45 rounded-tl-sm bg-border shadow-md" />
   </NavigationMenuPrimitive.Indicator>
